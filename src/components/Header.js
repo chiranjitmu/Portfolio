aee3066dcd6a8c729d9028d1c7bcc228b31a1284
Chiranjitprojects <Chiranjitmukherjee2002@gmail.com>
2023-01-23 18:22:15 +0530

    3

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,13 @@ function Header() {
   return (
     <div className='Header'>
         <h1 className='portfolio-logo'>PORTFOLIO</h1>
+
         <a className='header-home' id='home' href='#home'>HOME</a>
         <a className='header-about' href='#about'>ABOUT</a>
         <a className='header-project' href='#project'>PROJECT</a>
         <a className='header-contact' href='#contact'>CONTACT</a>
+
+
         <hr className='line-1'></hr>
        </div>
 
